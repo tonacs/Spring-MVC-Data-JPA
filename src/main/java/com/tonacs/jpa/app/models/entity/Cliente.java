cd09657bf8e097f4a895043d031a8e3761d039fb
tonacs <chilakil0510@gmail.com>
2021-05-11 20:10:50 -0500

    Primera versión del CRUD

---
+++ src/main/java/com/tonacs/jpa/app/models/entity/Cliente.java
@@ -0,0 +1,97 @@
+package com.tonacs.jpa.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// Si deseamos especificar el nombre en la tabla @Column(name =
+	// "NOMBRE_CLIENTE",)
+	
+	@NotEmpty
+	private String nombre;
+	@NotEmpty
+	private String apellido;
+	@NotEmpty
+	@Email
+	private String email;
+	
+	@NotNull
+	@Column(name = "CREATE_AT")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createAt;
+	
+	/*
+	@PrePersist
+	public void prePersist() {
+		createAt=new Date();
+	}
+	*/
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, CREATE_AT) VALUES(1, 'tona', 'c', 'tdev@tdev.com', '2017-11-11');
+INSERT INTO clientes (id, nombre, apellido, email, CREATE_AT) VALUES(2, 'dev1', 'jr', 'dev1@gmail.com', '2017-05-05');
