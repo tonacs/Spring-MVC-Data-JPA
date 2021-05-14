5c4d791cade886a32d30cdb58ed8751d12260842
tonacs <chilakil0510@gmail.com>
2021-05-14 02:34:48 -0500

    Se agregó paginación

--- src/main/java/com/tonacs/jpa/app/controllers/ClienteController.java
+++ src/main/java/com/tonacs/jpa/app/controllers/ClienteController.java
@@ -5,14 +5,21 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.tonacs.jpa.app.controllers.util.paginator.PageRender;
 import com.tonacs.jpa.app.models.entity.Cliente;
 import com.tonacs.jpa.app.models.service.IClienteService;
 
@@ -24,9 +31,15 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name = "page",defaultValue = "0")int page, Model model) {
+		
+		Pageable pageRequest=PageRequest.of(page, 5);
+		Page<Cliente> clientes=clienteService.findAll(pageRequest);
+		PageRender<Cliente> pageRender=new PageRender<>("/listar", clientes);
+		
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
 		return "listar";
 	}
 	
@@ -39,11 +52,16 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value = "id")Long id ,Map<String,Object> model) {
+	public String editar(@PathVariable(value = "id")Long id ,Map<String,Object> model, RedirectAttributes flash) {
 		Cliente cliente=null;
 		if(id>0) {
 			cliente=clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El id del cliente no EXISTE");
+				return "redirect:/listar";
+			}
 		}else {
+			flash.addFlashAttribute("error", "El id del cliente no puede ser 0");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
@@ -55,20 +73,23 @@ public class ClienteController {
 	
 	
 	@RequestMapping(value = "/form",method= RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result,Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result,Model model,RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
-			model.addAttribute("titulo", "formulario de cliente");
+			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
+		String mensajeFlash=(cliente.getId() != null)? "Cliente editado con éxito": "Cliente creado con éxito";	
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
 	
 	@RequestMapping(value = "/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if(id>0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente ELIMINADO con éxito");
 		}
 		
 		return "redirect:/listar";
---
+++ src/main/java/com/tonacs/jpa/app/controllers/util/paginator/PageRender.java
@@ -0,0 +1,79 @@
+package com.tonacs.jpa.app.controllers.util.paginator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+public class PageRender<T> {
+	private String url;
+	private Page<T> page;
+	private int totalPaginas;
+	private int numeroElementosPagina;
+	private int paginaActual;
+	private List<PageItem> paginas;
+
+	public PageRender(String url, Page<T> page) {
+		super();
+		this.url = url;
+		this.page = page;
+		this.paginas = new ArrayList<PageItem>();
+
+		numeroElementosPagina = page.getSize();
+		totalPaginas = page.getTotalPages();
+		paginaActual = page.getNumber() + 1;
+		int desde, hasta;
+		if (totalPaginas <= numeroElementosPagina) {
+			desde = 1;
+			hasta = totalPaginas;
+		} else {
+			if (paginaActual <= numeroElementosPagina / 2) {
+				desde = 1;
+				hasta = numeroElementosPagina;
+			} else if (paginaActual >= totalPaginas - numeroElementosPagina / 2) {
+				desde = totalPaginas - numeroElementosPagina + 1;
+				hasta = numeroElementosPagina;
+			} else {
+				desde = paginaActual - numeroElementosPagina / 2;
+				hasta = numeroElementosPagina;
+
+			}
+		}
+		for (int i = 0; i < hasta; i++) {
+			paginas.add(new PageItem(desde + i, paginaActual == desde + i));
+		}
+
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public int getTotalPaginas() {
+		return totalPaginas;
+	}
+
+	public int getPaginaActual() {
+		return paginaActual;
+	}
+
+	public List<PageItem> getPaginas() {
+		return paginas;
+	}
+	
+	public boolean isFirst() {
+		return page.isFirst();
+	}
+	public boolean isLast() {
+		return page.isLast();
+	}
+	
+	public boolean isHasNext() {
+		return page.hasNext();
+	}
+	public boolean isHasPrevious() {
+		return page.hasPrevious();
+	}
+
+}
